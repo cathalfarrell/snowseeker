e8fd372b1bd8ff9f6918d074a23059c4ad3092e2
Cathal Farrell <cathalproductions@gmail.com>
2020-08-10 15:33:00 +0100

    Challenge 2 - Fill in the loading and saving methods for Favourites.

--- SnowSeeker/Favourites.swift
+++ SnowSeeker/Favourites.swift
@@ -16,6 +16,7 @@ class Favourites: ObservableObject {
     init() {
         //load our saved data
         self.resorts = []
+        loadData()
     }
 
     // returns true if our set contains this resort
@@ -39,6 +40,32 @@ class Favourites: ObservableObject {
 
     func save() {
         // write out our data
+
+        print("Saving data...")
+
+        do {
+            let data = try JSONEncoder().encode(resorts)
+            UserDefaults.standard.set(data, forKey: saveKey)
+            print("Saved your \(self.resorts.count) favourite resorts.")
+        } catch (let error) {
+            print("Error on saving: \(error.localizedDescription)")
+        }
+    }
+
+    func loadData() {
+        // load our data
+
+        print("Loading data...")
+
+        do {
+            if let data = UserDefaults.standard.data(forKey: saveKey) {
+                let decoded = try JSONDecoder().decode(Set<String>.self, from: data)
+                self.resorts = decoded
+                print("Loaded your \(self.resorts.count) favourite resorts.")
+            }
+        } catch (let error) {
+            print("Error on saving: \(error.localizedDescription)")
+        }
     }
 
 
